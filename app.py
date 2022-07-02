fb488603b55d0def2a493401278032452f27baa1
Chris <65775571+Birrellc@users.noreply.github.com>
2022-07-02 18:44:24 +0100

    Feat: added user friendly equation in response

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Import required modules
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from decimal import Decimal
+import math
+import statistics
 
 # instantiate Flask application.
 app = Flask(__name__)
@@ -10,29 +12,37 @@ app = Flask(__name__)
 
 @app.route('/calc/web/add/<x>/to/<y>', methods=['GET'])
 def add(x, y):
-    return make_response(str(Decimal(x) + Decimal(y)), 200)
+    ans = (Decimal(x) + Decimal(y))
+    equation = f'{str(x)} + {str(y)} = {str(ans)}'
+    return make_response(equation, 200)
 
 # returns the difference of path parameters x and y
 
 
 @app.route('/calc/web/subtract/<x>/from/<y>', methods=['GET'])
 def subtract(x, y):
-    return make_response(str(Decimal(y) - Decimal(x)), 200)
+    ans = (Decimal(x) - Decimal(y))
+    equation = f'{str(x)} - {str(y)} = {str(ans)}'
+    return make_response(equation, 200)
 
 # returns the product of path parameters x and y
 
 
 @app.route('/calc/web/multiply/<x>/by/<y>', methods=['GET'])
 def multiply(x, y):
-    return make_response(str(Decimal(x) * Decimal(y)), 200)
+    ans = (Decimal(x) * Decimal(y))
+    equation = f'{str(x)} * {str(y)} = {str(ans)}'
+    return make_response(equation, 200)
 
 # returns the quotient of path parameters x and y and returns error if y is 0
 
 
 @app.route('/calc/web/divide/<x>/by/<y>', methods=['GET'])
 def divide(x, y):
+    ans = (Decimal(x) / Decimal(y))
+    equation = f'{str(x)} / {str(y)} = {str(ans)}'
     try:
-        return make_response(str(int(x) / int(y)), 200)
+        return make_response(equation, 200)
     except ZeroDivisionError:
         # return Cannot divide by zero error
         return make_response("Cannot divide by zero", 400)
